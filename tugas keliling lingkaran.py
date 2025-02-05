927997a10cf5a079bdc4e947fe6d0c9f14a46063
afifamirdn <syamsoe20@gmail.com>
2025-02-05 13:41:06 +0700

    Create tugas keliling lingkaran.py

---
+++ tugas keliling lingkaran.py
@@ -0,0 +1,9 @@
+def keliling_lingkaran(r):
+    return 2 * 3.14 * r
+
+# Input dari pengguna
+r = float(input("Masukkan jari-jari lingkaran: "))
+
+# Hitung dan tampilkan hasil
+keliling = keliling_lingkaran(r)
+print(f"Keliling lingkaran dengan jari-jari {r} adalah {keliling:}")
